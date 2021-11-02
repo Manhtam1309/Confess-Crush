cec11f2450824db3c32674353b5815feb719c23f
Manhtam1309 <93583664+Manhtam1309@users.noreply.github.com>
2021-11-02 17:27:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
     introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Tớ có điều này muốn hỏi
+    Cậu phải trả lời thật lòng nha
+    ( Hứa phải thật lòng đó nha , 
+    xạo là nghỉ chơi đó ),
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: ' Nếu cậu không trả lời mà thoát ra thì coi như đông ý làm ny mình  🥰',
+    desc: 'Còn nếu im lặng là đồng ý nha 😁  ',
+    btnYes: ' sao cậu biết <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Đồng ý thì mình dọn đồ vào ở luôn ha 😂😁',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
